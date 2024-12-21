01b3ab270c7237a933bd5a58300cae0e925e37ad
Vick-glomba <126066706+Vick-glomba@users.noreply.github.com>
2024-12-21 16:12:11 -0300

    fic8

--- public/index.js
+++ public/index.js
@@ -772,8 +772,9 @@ const actualizarInventario = () => {
           const item = myPlayer.inventario[contador][0]
           const url = "./items/" + dbItems[item].imagen + ".bmp"
           const url2 = "./items/" + dbItems[item].imagen + ".BMP"
-          const existe= checkUrl(url);
-          imagen = existe?`background-image: url(${url});`:`background-image: url(${url2});`
+        //  const existe= checkUrl(url);
+          const image = require(url)
+          imagen = image?`background-image: url(${url});`:`background-image: url(${url2});`
           if (itemSelect === "slot" + contador) {
             borde = "border-color: rgb(253, 232, 0);"
           } else {
