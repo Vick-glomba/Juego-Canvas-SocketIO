5e96f33ab61085931c65e366797ae74c6c376180
Vick-glomba <126066706+Vick-glomba@users.noreply.github.com>
2024-12-22 14:13:04 -0300

    a

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const loadPj = require("./pjLoader");
 const db = require("./hechizosDB");
 const dbItems = db.items
 const SPEED = 6;
-const FPS =20;
+const FPS =18;
 
 const PLAYER_SIZE = 120;
 const TILE_SIZE = 32;
