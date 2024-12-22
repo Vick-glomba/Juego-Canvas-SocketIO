786f9e1c0696943bf71050c82ebe3788ea5ee0fa
Vick-glomba <126066706+Vick-glomba@users.noreply.github.com>
2024-12-22 12:41:52 -0300

    a
    a

--- public/index.js
+++ public/index.js
@@ -1934,22 +1934,24 @@ setInterval(() => {
   socket.emit("myPlayer", player => {
     myPlayer = player
     
+    if(myPlayer){
+      cameraX = parseInt(myPlayer.x - canvasEl.width / 2);
+      cameraY = parseInt(myPlayer.y - canvasEl.height / 2)
+
+    }
     socket.emit("enMapa", myPlayer.mapa, ({ playersEnMapa, snowballsEnMapa, playersOnlines }) => {
       
       players = playersEnMapa
       itemsEnMapa = players.filter(p => p.skin === "items")
+      myPlayer = players.find((player) => player.id === socket.id);
+      players.sort(((a, b) => a.y - b.y))
       snowballs = snowballsEnMapa
       
       playersOnline = playersOnlines
-      
-      
-      
+
+
     })
   })
-  myPlayer = players.find((player) => player.id === socket.id);
-  players.sort(((a, b) => a.y - b.y))
-  cameraX = parseInt(myPlayer.x - canvasEl.width / 2);
-  cameraY = parseInt(myPlayer.y - canvasEl.height / 2)
   loop();
 
 }, 1000 / FPS);
